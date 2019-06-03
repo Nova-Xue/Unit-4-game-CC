454372d8713534eb5bc36165ddbdc4e7bef01a58
Nova <xueshengyuan1118@hotmail.com>
2019-06-03 16:42:45 -0400

    Add click action and isResulted to disable the click after there is a result

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,92 @@
-$(document).ready(function(){
+$(document).ready(function () {
     var winCount = 0;
     var lossCount = 0;
     var guessCurrent = 0;
+    var guessNum = 0;
     var numArray = [];
-    function getRandomGuess (){//get 6 random number from 1-15 and put in a set
+    var isResulted = false;
+    function getRandomGuess() {//get 6 random number from 1-15 and put in a set
         var numSet = new Set();
-        var guessNum =0;
-        for(;;){
-            numSet.add(Math.floor(Math.random()*15+1));
-            if(numSet.size == 6) break;
+        for (; ;) {
+            numSet.add(Math.floor(Math.random() * 15 + 1));
+            if (numSet.size == 6) break;
         }
-       numArray = Array.from(numSet);//numbers with index in numArray
-        for(var i = 0;i<numArray.length;i++){
-            var ranTimes = Math.floor(Math.random()*3+1);
-            guessNum += numArray[i]*ranTimes;
+        numArray = Array.from(numSet);//numbers with index in numArray
+        for (var i = 0; i < numArray.length; i++) {
+            var ranTimes = Math.floor(Math.random() * 3 + 1);
+            guessNum += numArray[i] * ranTimes;
         }
-       return guessNum;
+        return guessNum;
     }
-    function reset(){
+    function reset() {
         $("#guessNum").text(getRandomGuess());
-        $("#guessCurrent").text(guessCurrent);
-        $("#wins").text(winCount);
-        $("#losses").text(lossCount);
-        for(var i = 0;i<numArray.length;i++){
-            $(".crys"+(i+1)+"-num").html(numArray[i]);
+        $("#guessCurrent").text("");
+        $(".result").text("");
+        guessCurrent = 0;
+        isResulted = false;
+        for (var i = 0; i < numArray.length; i++) {
+            $(".crys" + i + "-num").html(numArray[i]);
         }
-        
-    } 
-    $(".reset").on("click",function(){
+
+    }
+    function showGuess() {
+        $("#guessCurrent").text(guessCurrent);
+    }
+    $(".reset").on("click", function () {
         reset();
     });
-    $(".crys").on("mousedown",function(){
-        $(this).children("img").hide();
+    $(".crys").on("mousedown", function () {
+        if (isResulted) {
+            return false;
+        } else {
+            $(this).children("img").hide();
+        }
+
     });
-    $(".crys").on("mouseup",function(){
-        $(this).children("img").show();
+    $(".crys").on("mouseup", function () {
+        if (isResulted) {
+            return false;
+        } else {
+            $(this).children("img").show();
+        }
     });
+    $(".crys").on("click", function () {
+        if (isResulted) {
+            return false;
+        } else {
+            var numIndex = $(this).children("img").attr("alt");
+            var num = numArray[(Number(numIndex))];
+            guessCurrent += num;
+            if (guessCurrent == guessNum) {
+                showGuess();
+                var img = $('<img>');
+                img.attr("src", "assets/images/iron-man-snap.png")
+                img.attr("class", "img-fluid rounded");
+                $(".result").append(img);
+                // $(".result").text("you win");
+                winCount++;
+                $("#wins").text(winCount);
+                isResulted = true;
+                alert("you win");
+            } else if (guessCurrent > guessNum) {
+                showGuess();
+                var img = $('<img>');
+                img.attr("src", "assets/images/thanos-snap.jpg");
+                img.attr("class", "img-fluid rounded");
+                $(".result").append(img);
+                // $(".result").text("you lose");
+                lossCount++;
+                $("#losses").text(lossCount);
+                isResulted = true;
+                alert("you lose");
+
+            } else {
+                showGuess();
+
+            }
+        }
+    });
+
     reset();
 
 });
