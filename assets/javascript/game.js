ddbdaf5fb225d2ae36366f530981bcf8794a01fe
Nova <xueshengyuan1118@hotmail.com>
2019-06-03 15:54:25 -0400

    Change jquery structure to be short in code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -30,71 +30,11 @@ $(document).ready(function(){
     $(".reset").on("click",function(){
         reset();
     });
-    $(".crys1").on("click",function(){
-
+    $(".crys").on("mousedown",function(){
+        $(this).children("img").hide();
     });
-    $(".crys1").on("mousedown",function(){
-        
-        $(".img1").hide();
-    });
-    $(".crys1").on("mouseup",function(){
-        $(".img1").show();
-
-    });
-    $(".crys2").on("click",function(){
-        
-    });
-    $(".crys2").on("mousedown",function(){
-        
-        $(".img2").hide();
-    });
-    $(".crys2").on("mouseup",function(){
-        $(".img2").show();
-
-    });
-    $(".crys3").on("click",function(){
-        
-    });
-    $(".crys3").on("mousedown",function(){
-        
-        $(".img3").hide();
-    });
-    $(".crys3").on("mouseup",function(){
-        $(".img3").show();
-
-    });
-    $(".crys4").on("click",function(){
-        
-    });
-    $(".crys4").on("mousedown",function(){
-        
-        $(".img4").hide();
-    });
-    $(".crys4").on("mouseup",function(){
-        $(".img4").show();
-
-    });
-    $(".crys5").on("click",function(){
-        
-    });
-    $(".crys5").on("mousedown",function(){
-        
-        $(".img5").hide();
-    });
-    $(".crys5").on("mouseup",function(){
-        $(".img5").show();
-
-    });
-    $(".crys6").on("click",function(){
-        
-    });
-    $(".crys6").on("mousedown",function(){
-        
-        $(".img6").hide();
-    });
-    $(".crys6").on("mouseup",function(){
-        $(".img6").show();
-
+    $(".crys").on("mouseup",function(){
+        $(this).children("img").show();
     });
     reset();
 
